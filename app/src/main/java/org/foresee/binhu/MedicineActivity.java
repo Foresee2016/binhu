25ab1cbda85bf8597a789df415a37a5eeab8b861
Foresee <1123520670@qq.com>
2018-09-22 20:45:48 +0800

    MedicineActivity extends SingleFragmentActivity use MedicineFragment.

--- app/src/main/java/org/foresee/binhu/MedicineActivity.java
+++ app/src/main/java/org/foresee/binhu/MedicineActivity.java
@@ -1,13 +1,13 @@
 package org.foresee.binhu;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MedicineActivity extends AppCompatActivity {
+public class MedicineActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medicine);
+    protected Fragment createFragment() {
+        return MedicineFragment.newInstance();
     }
 }
---
+++ app/src/main/java/org/foresee/binhu/MedicineFragment.java
@@ -0,0 +1,9 @@
+package org.foresee.binhu;
+
+import android.support.v4.app.Fragment;
+
+public class MedicineFragment extends Fragment {
+    public static MedicineFragment newInstance(){
+        return new MedicineFragment();
+    }
+}
