748175852338045050acb23afae549c95dec6aa4
Foresee <1123520670@qq.com>
2018-09-23 18:23:40 +0800

    Use DrawerLayout define left slide view.

--- app/src/main/java/org/foresee/binhu/MainActivity.java
+++ app/src/main/java/org/foresee/binhu/MainActivity.java
@@ -1,13 +1,60 @@
 package org.foresee.binhu;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends SingleFragmentActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MedicineFragment.Callbacks{
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private BottomNavigationView mBottomNavigationView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout=findViewById(R.id.main_drawer);
+        mActionBarDrawerToggle=new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name, R.string.app_name);
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment contentFrame=fm.findFragmentById(R.id.content_frame);
+        if(contentFrame==null){
+            contentFrame=MedicineFragment.newInstance(this);
+            fm.beginTransaction().add(R.id.content_frame, contentFrame).commit();
+        }
+        Fragment drawerFrame=fm.findFragmentById(R.id.drawer_frame);
+        if(drawerFrame==null){
+            drawerFrame=DrawerFragment.newInstance();
+            fm.beginTransaction().add(R.id.drawer_frame, drawerFrame).commit();
+        }
+    }
 
     @Override
-    protected Fragment createFragment() {
-        return MedicineFragment.newInstance();
+    public void onToolbarNavClick() {
+        mDrawerLayout.openDrawer(GravityCompat.START);
     }
+
 }
--- app/src/main/java/org/foresee/binhu/MedicineFragment.java
+++ app/src/main/java/org/foresee/binhu/MedicineFragment.java
@@ -1,9 +1,93 @@
 package org.foresee.binhu;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MedicineFragment extends Fragment {
-    public static MedicineFragment newInstance(){
-        return new MedicineFragment();
+    public static MedicineFragment newInstance(Callbacks callbacks) {
+        MedicineFragment fragment=new MedicineFragment();
+        fragment.setCallbacks(callbacks);
+        return fragment;
+    }
+
+    private static final String ARGS_CALLBACKS="CallBacks";
+    private Callbacks mCallbacks;
+
+    private Toolbar mToolbar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mToolbar = view.findViewById(R.id.tool_bar);
+        initToolbar();
+//        mBottomNavigationView = view.findViewById(R.id.nav_bottom);
+//        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.medicine:
+//                        Toast.makeText(getActivity(), "PK", Toast.LENGTH_SHORT).show();
+//                        return true;
+//                    case R.id.prescription:
+//                        Toast.makeText(getActivity(), "主页", Toast.LENGTH_SHORT).show();
+//                        return true;
+//                    case R.id.sphygmology:
+//                        Toast.makeText(getActivity(), "我的", Toast.LENGTH_SHORT).show();
+//                        return true;
+//                    case R.id.channel:
+//                        Toast.makeText(getActivity(), "我的", Toast.LENGTH_SHORT).show();
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+//            }
+//        });
+        return view;
+    }
+
+    private void initToolbar() {
+        mToolbar.inflateMenu(R.menu.toolbar_main);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.read_pic:
+                        Toast.makeText(getActivity(), "Read Pic", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.update:
+                        Toast.makeText(getActivity(), "Update data", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.search:
+                        Toast.makeText(getActivity(), "Search Medicine", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallbacks.onToolbarNavClick();
+            }
+        });
+    }
+
+    public void setCallbacks(Callbacks callbacks) {
+        mCallbacks = callbacks;
+    }
+
+    public interface Callbacks{
+        void onToolbarNavClick();
     }
 }
